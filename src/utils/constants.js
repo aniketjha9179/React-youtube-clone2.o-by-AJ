16fba6af6abfe81f080e862504f723ec9ecfffbc
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2022-11-30 09:56:12 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,8 +12,10 @@ import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
   // { name: 'JS Mastery', icon: <CodeIcon />, },
