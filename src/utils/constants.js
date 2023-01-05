f5ba16fbd4010fcacfd13fda78f85aa5bd156c58
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2023-01-05 12:23:08 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,13 +12,10 @@ import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
-
-
-
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
+ // { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
